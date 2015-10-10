d2f6725345121b63b8329a7aba9fa8a76065a791
Mikron <mikronia@gmail.com>
2015-10-10 17:10:26 +0200

    Test for Person rep list

--- tests/domain/PersonTest.php
+++ tests/domain/PersonTest.php
@@ -1,6 +1,9 @@
 <?php
 
 use Mikron\ReputationList\Domain\Entity\Person;
+use Mikron\ReputationList\Domain\Entity\Reputation;
+use Mikron\ReputationList\Domain\Entity\ReputationEvent;
+use Mikron\ReputationList\Domain\ValueObject\ReputationNetwork;
 
 class PersonTest extends PHPUnit_Framework_TestCase
 {
@@ -21,6 +24,18 @@ class PersonTest extends PHPUnit_Framework_TestCase
         $person = new Person(1, "Test Name", "Test Description", []);
         $this->assertEquals("Test Name", $person->getName());
     }
+    
+    /**
+     * @test
+     * @dataProvider reputationDataProvider
+     * @param $reputations
+     */
+    public function areReputationsCorrectType($reputations)
+    {
+        $person = new Person(1, "Test Name", "Test Description", $reputations);
+        $this->assertContainsOnlyInstancesOf("Mikron\\ReputationList\\Domain\\Entity\\Reputation",
+            $person->getReputations());
+    }
 
     /**
      * @test
@@ -29,6 +44,38 @@ class PersonTest extends PHPUnit_Framework_TestCase
     {
         $person = new Person(1, "Test Name", "Test Description", []);
         $this->assertEquals(["name" => "Test Name"], $person->getSimpleData());
+    }
+
+    public function reputationDataProvider()
+    {
+        $repNetCivil = new ReputationNetwork("c", ["name" => "CivicNet", "description" => "Corporations"]);
+        $repNetMilitary = new ReputationNetwork("m", ["name" => "MilNet", "description" => "Mercenaries"]);
+
+        return [
+            [
+                [
+                    new Reputation($repNetMilitary, [
+                        new ReputationEvent(null, $repNetMilitary, 2, null),
+                        new ReputationEvent(null, $repNetMilitary, 3, null),
+                    ]),
+                    new Reputation($repNetCivil, [
+                        new ReputationEvent(null, $repNetCivil, 1, null),
+                        new ReputationEvent(null, $repNetCivil, 2, null),
+                    ])
+                ]
+            ],
+            [
+                [
+                    new Reputation($repNetMilitary, [
+                        new ReputationEvent(null, $repNetMilitary, 2, null),
+                        new ReputationEvent(null, $repNetMilitary, 3, null),
+                    ])
+                ]
+            ],
+            [
+                []
+            ]
+        ];
     }
 
 }

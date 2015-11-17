d3b9b86b4378188e01edacb13fac728522be23ce
Mikron <mikronia@gmail.com>
2015-11-17 22:44:25 +0100

    Database handling for events

--- src/Infrastructure/Factory/Event.php
+++ src/Infrastructure/Factory/Event.php
@@ -3,6 +3,7 @@
 namespace Mikron\ReputationList\Infrastructure\Factory;
 
 use Mikron\ReputationList\Domain\Entity;
+use Mikron\ReputationList\Infrastructure\Storage\StorageForEvent;
 
 class Event
 {
@@ -41,5 +42,55 @@ class Event
         return $list;
     }
 
+    /**
+     * Retrieves Event objects from database
+     *
+     * @param StorageEngine $connection
+     * @return array
+     */
+    public function retrieveAllFromDb($connection)
+    {
+        $eventStorage = new StorageForEvent($connection);
 
+        $array = $eventStorage->retrieveAll();
+
+        if (!empty($array)) {
+            foreach ($array as $record) {
+                $list[] = $this->createFromSingleArray(
+                    $record['event_id'],
+                    $record['name'],
+                    $record['description']
+                );
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * @param StorageEngine $connection
+     * @param int $dbId
+     * @return Entity\Event
+     * @throws EventNotFoundException
+     */
+    public function retrieveEventFromDbById($connection, $dbId)
+    {
+        $eventStorage = new StorageForEvent($connection);
+
+        $eventWrapped = $eventStorage->retrieveById($dbId);
+
+        if (!empty($eventWrapped)) {
+            $eventUnwrapped = array_pop($eventWrapped);
+
+            $event = $this->createFromSingleArray(
+                $eventUnwrapped['event_id'],
+                $eventUnwrapped['name'],
+                $eventUnwrapped['description']
+            );
+        } else {
+            throw new EventNotFoundException("Event with given ID has not been found in our database");
+        }
+
+        return $event;
+    }
 }
---
+++ src/Infrastructure/Storage/StorageForEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Mikron\ReputationList\Infrastructure\Storage;
+
+use Mikron\ReputationList\Domain\Blueprint\StorageEngine;
+use Mikron\ReputationList\Domain\Storage\StorageForObject;
+
+final class StorageForEvent implements StorageForObject
+{
+    /**
+     * @var StorageEngine
+     */
+    private $storage;
+
+    /**
+     * MySqlevent constructor.
+     * @param $storage
+     */
+    public function __construct($storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public function retrieveById($dbId)
+    {
+        $result = $this->storage->selectByPrimaryKey('event', 'event_id', [$dbId]);
+
+        return $result;
+    }
+
+    public function retrieveAll()
+    {
+        $result = $this->storage->selectAll('event', 'event_id');
+
+        return $result;
+    }
+
+    public function retrieveByKey($key)
+    {
+        throw new KeyNotSupportedException("Event does not support key use. Please use database ID");
+    }
+}

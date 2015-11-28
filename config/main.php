939101e9133c213cb55ab373775044111b6b6a96
Mikron <mikronia@gmail.com>
2015-11-28 16:30:35 +0100

    Version adjusted

--- config/main.php
+++ config/main.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "version" => "0-dev",
+    "version" => "0.3.0",
     "interface" => [
         "title" => "Reputation Board",
         "welcome" => "Welcome to Reputation Board",

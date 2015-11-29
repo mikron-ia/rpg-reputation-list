86d79711d9d4e671c589fec48ebf2607b8c89670
Mikron <mikronia@gmail.com>
2015-11-29 15:36:38 +0100

    System label added, 7th Sea config file added

---
+++ config/data/7th.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Data file for 7th Sea RPG system, a creation of John Wick, originally published by Alderac Entertainment Group
+ * http://www.alderac.com/7thsea/
+ * No copyright infringement intended, no copyrighted content used
+ */
+return [
+    'systemLabel' => 'SeventhSea',
+    'systemData' => [
+        /**
+         * Due to copyrighted and highly limited nature of existing data, no reputations are listed here
+         * Should you have created your own system for managing reputation in 7th Sea system, please place it in the `epic.php` config file
+         */
+        'reputations' => []
+    ],
+];
--- config/data/ep.php
+++ config/data/ep.php
@@ -5,9 +5,8 @@
  * http://eclipsephase.com/
  * Data used in accord with http://creativecommons.org/licenses/by-nc-sa/3.0/us/
  */
-
 return [
-    'systemLabel' => 'ep',
+    'systemLabel' => 'EclipsePhase',
     'systemData' => [
         'reputations' => [
             "@" => [

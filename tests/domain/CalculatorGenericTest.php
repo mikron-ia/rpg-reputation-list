66effe71b6fc07b4390d0bc276b704708e835cf1
Mikron <mikronia@gmail.com>
2015-12-09 22:04:18 +0100

    Tests for generic calculator

---
+++ tests/domain/CalculatorGenericTest.php
@@ -0,0 +1,104 @@
+<?php
+
+use Mikron\ReputationList\Domain\Service\CalculatorGeneric;
+
+class CalculatorGenericTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param int[] $values
+     * @param int[] $currentState
+     * @param int $lowest
+     * @param int $highest
+     * @param int $abs
+     */
+    public function calculateLowestAndHighestCalculatesCorrectly($values, $currentState, $lowest, $highest, $abs)
+    {
+        $expectation = [
+            'negativeMin' => $lowest,
+            'positiveMax' => $highest
+        ];
+
+        $result = CalculatorGeneric::calculateLowestAndHighest($values, $currentState);
+
+        $this->assertEquals($expectation, $result);
+    }
+
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param int[] $values
+     * @param int[] $currentState
+     * @param int $lowest
+     * @param int $highest
+     * @param int $abs
+     */
+    public function calculateAbsoluteCalculatesCorrectly($values, $currentState, $lowest, $highest, $abs)
+    {
+        $expectation = [
+            'absolute' => $abs
+        ];
+
+        $result = CalculatorGeneric::calculateAbsolute($values, $currentState);
+
+        $this->assertEquals($expectation, $result);
+    }
+
+    public function valuesProvider()
+    {
+        return [
+            [
+                [0],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                0,
+                0,
+                0
+            ],
+            [
+                [0, 0],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                0,
+                0,
+                0
+            ],
+            [
+                [-5, 5],
+                ['balance' => 0, 'negative' => -5, 'positive' => 5],
+                -5,
+                0,
+                10
+            ],
+            [
+                [5, -5],
+                ['balance' => 0, 'negative' => -5, 'positive' => 5],
+                0,
+                5,
+                10
+            ],
+            [
+                [-10, 5],
+                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                -10,
+                0,
+                15
+            ],
+            [
+                [5, -10],
+                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                -5,
+                5,
+                15
+            ],
+            [
+                [-10, 5],
+                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                -10,
+                0,
+                15
+            ],
+        ];
+    }
+}

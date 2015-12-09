5c0d1a2747dad3c8a0ec4b87196759217435f1ff
Mikron <mikronia@gmail.com>
2015-12-10 00:21:13 +0100

    Tests for basic calculator

--- tests/domain/CalculatorGenericTest.php
+++ tests/domain/CalculatorGenericTest.php
@@ -10,17 +10,23 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
      *
      * @param int[] $values
      * @param int[] $currentState
+     * @param int[] $basics
      * @param int $lowest
      * @param int $highest
      * @param int $abs
      */
-    public function calculateLowestAndHighestCalculatesCorrectly($values, $currentState, $lowest, $highest, $abs)
-    {
+    public function calculateLowestAndHighestCalculatesCorrectly(
+        $values,
+        $currentState,
+        $basics,
+        $lowest,
+        $highest,
+        $abs
+    ) {
         $expectation = [
             'negativeMin' => $lowest,
             'positiveMax' => $highest
         ];
-
         $result = CalculatorGeneric::calculateLowestAndHighest($values, $currentState);
 
         $this->assertEquals($expectation, $result);
@@ -32,16 +38,37 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
      *
      * @param int[] $values
      * @param int[] $currentState
+     * @param int[] $basics
      * @param int $lowest
      * @param int $highest
      * @param int $abs
      */
-    public function calculateAbsoluteCalculatesCorrectly($values, $currentState, $lowest, $highest, $abs)
+    public function calculateBasicsCalculatesCorrectly($values, $currentState, $basics, $lowest, $highest, $abs)
+    {
+        $expectation = $basics;
+        $result = CalculatorGeneric::calculateSimple($values, $currentState);
+
+        $this->assertEquals($expectation, $result);
+    }
+
+    /**
+     * @test
+     * @dataProvider valuesProvider
+     *
+     * @param int[] $values
+     * @param int[] $currentState
+     * @param int[] $basics
+     * @param int $lowest
+     * @param int $highest
+     * @param int $abs
+     * @internal param int $negative
+     * @internal param int $positive
+     */
+    public function calculateAbsoluteCalculatesCorrectly($values, $currentState, $basics, $lowest, $highest, $abs)
     {
         $expectation = [
             'absolute' => $abs
         ];
-
         $result = CalculatorGeneric::calculateAbsolute($values, $currentState);
 
         $this->assertEquals($expectation, $result);
@@ -53,6 +80,7 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
             [
                 [0],
                 ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
                 0,
                 0,
                 0
@@ -60,12 +88,14 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
             [
                 [0, 0],
                 ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
                 0,
                 0,
                 0
             ],
             [
                 [-5, 5],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
                 ['balance' => 0, 'negative' => -5, 'positive' => 5],
                 -5,
                 0,
@@ -73,6 +103,7 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
             ],
             [
                 [5, -5],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
                 ['balance' => 0, 'negative' => -5, 'positive' => 5],
                 0,
                 5,
@@ -80,21 +111,24 @@ class CalculatorGenericTest extends PHPUnit_Framework_TestCase
             ],
             [
                 [-10, 5],
-                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                ['balance' => -5, 'negative' => -10, 'positive' => 5],
                 -10,
                 0,
                 15
             ],
             [
                 [5, -10],
-                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                ['balance' => -5, 'negative' => -10, 'positive' => 5],
                 -5,
                 5,
                 15
             ],
             [
                 [-10, 5],
-                ['balance' => 0, 'negative' => -10, 'positive' => 5],
+                ['balance' => 0, 'negative' => 0, 'positive' => 0],
+                ['balance' => -5, 'negative' => -10, 'positive' => 5],
                 -10,
                 0,
                 15

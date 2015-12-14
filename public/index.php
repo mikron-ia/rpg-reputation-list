718914b166734d2d671b65733b206c0b252e5647
Mikron <mikronia@gmail.com>
2015-12-14 15:09:43 +0100

    API path changed for less-error prone form

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application;
 
-$apiPath = '/../api/';
+$apiPath = __DIR__ . '/../api/';
 
 require_once $apiPath.'config.php';
 require_once $apiPath.'dic.php';
